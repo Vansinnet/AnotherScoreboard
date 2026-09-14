205777577877e48f0e51c38ca60b6fcbcc247c9b
Keaun <liukangcc@outlook.com>
2026-09-14 21:08:05 +0800

    add Chinese description

--- scripts/mods/AnotherScoreboard/AnotherScoreboard_enemies.lua
+++ scripts/mods/AnotherScoreboard/AnotherScoreboard_enemies.lua
@@ -3,13 +3,19 @@ return {
         {
             stat_id = "ranged_lesser_enemies_killed",
             label = "row_ranged_lesser_enemies_killed",
-            name = "Ranged Lesser Enemies",
+            name = {
+                en = "Ranged Lesser Enemies",
+                ["zh-cn"] = "远程普通敌人",
+            },
             breeds = { "cultist_assault", "renegade_assault", "renegade_rifleman" },
         },
         {
             stat_id = "melee_lesser_enemies_killed",
             label = "row_melee_lesser_enemies_killed",
-            name = "Melee Lesser Enemies",
+            name = {
+                en = "Melee Lesser Enemies",
+                ["zh-cn"] = "近战普通敌人",
+            },
             breeds = {
                 "chaos_newly_infected",
                 "chaos_poxwalker",
@@ -27,43 +33,64 @@ return {
         {
             stat_id = "special_pox_hound_killed",
             label = "row_special_pox_hound",
-            name = "Pox Hounds",
+            name = {
+                en = "Pox Hounds",
+                ["zh-cn"] = "瘟疫猎犬",
+            },
             breeds = { "chaos_hound", "chaos_hound_mutator", "chaos_armored_hound" },
         },
         {
             stat_id = "special_mutant_killed",
             label = "row_special_mutant",
-            name = "Mutants",
+            name = {
+                en = "Mutants",
+                ["zh-cn"] = "变种人",
+            },
             breeds = { "cultist_mutant", "cultist_mutant_mutator" },
         },
         {
             stat_id = "special_trapper_killed",
             label = "row_special_trapper",
-            name = "Trappers",
+            name = {
+                en = "Trappers",
+                ["zh-cn"] = "陷阱手",
+            },
             breeds = { "renegade_netgunner" },
         },
         {
             stat_id = "special_poxburster_killed",
             label = "row_special_poxburster",
-            name = "Poxbursters",
+            name = {
+                en = "Poxbursters",
+                ["zh-cn"] = "瘟疫爆破者",
+            },
             breeds = { "chaos_poxwalker_bomber" },
         },
         {
             stat_id = "special_bomber_killed",
             label = "row_special_bomber",
-            name = "Bombers",
+            name = {
+                en = "Bombers",
+                ["zh-cn"] = "自爆者",
+            },
             breeds = { "renegade_grenadier", "cultist_grenadier" },
         },
         {
             stat_id = "special_sniper_killed",
             label = "row_special_sniper",
-            name = "Snipers",
+            name = {
+                en = "Snipers",
+                ["zh-cn"] = "狙击手",
+            },
             breeds = { "renegade_sniper" },
         },
         {
             stat_id = "special_flamer_killed",
             label = "row_special_flamer",
-            name = "Flamers",
+            name = {
+                en = "Flamers",
+                ["zh-cn"] = "火焰兵",
+            },
             breeds = { "renegade_flamer", "renegade_flamer_mutator", "cultist_flamer" },
         },
     },
@@ -71,49 +98,73 @@ return {
         {
             stat_id = "elite_rager_killed",
             label = "row_elite_rager",
-            name = "Ragers",
+            name = {
+                en = "Ragers",
+                ["zh-cn"] = "狂战士",
+            },
             breeds = { "cultist_berzerker", "renegade_berzerker" },
         },
         {
             stat_id = "elite_mauler_killed",
             label = "row_elite_mauler",
-            name = "Maulers",
+            name = {
+                en = "Maulers",
+                ["zh-cn"] = "重锤兵",
+            },
             breeds = { "renegade_executor" },
         },
         {
             stat_id = "elite_bulwark_killed",
             label = "row_elite_bulwark",
-            name = "Bulwarks",
+            name = {
+                en = "Bulwarks",
+                ["zh-cn"] = "盾卫",
+            },
             breeds = { "chaos_ogryn_bulwark" },
         },
         {
             stat_id = "elite_crusher_killed",
             label = "row_elite_crusher",
-            name = "Crushers",
+            name = {
+                en = "Crushers",
+                ["zh-cn"] = "粉碎者",
+            },
             breeds = { "chaos_ogryn_executor" },
         },
         {
             stat_id = "elite_gunner_killed",
             label = "row_elite_gunner",
-            name = "Gunners",
+            name = {
+                en = "Gunners",
+                ["zh-cn"] = "枪手",
+            },
             breeds = { "cultist_gunner", "renegade_gunner" },
         },
         {
             stat_id = "elite_shotgunner_killed",
             label = "row_elite_shotgunner",
-            name = "Shotgunners",
+            name = {
+                en = "Shotgunners",
+                ["zh-cn"] = "霰弹兵",
+            },
             breeds = { "cultist_shocktrooper", "renegade_shocktrooper" },
         },
         {
             stat_id = "elite_reaper_killed",
             label = "row_elite_reaper",
-            name = "Reapers",
+            name = {
+                en = "Reapers",
+                ["zh-cn"] = "收割者",
+            },
             breeds = { "chaos_ogryn_gunner" },
         },
         {
             stat_id = "elite_plasma_gunner_killed",
             label = "row_elite_plasma_gunner",
-            name = "Plasma Gunners",
+            name = {
+                en = "Plasma Gunners",
+                ["zh-cn"] = "等离子枪手",
+            },
             breeds = { "renegade_plasma_gunner" },
         },
     },
